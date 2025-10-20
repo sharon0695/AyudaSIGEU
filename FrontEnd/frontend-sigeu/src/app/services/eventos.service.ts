73f234b2ade4f5a70f71007a5aaa4e038fb17b44
sharon0695 <shzuray@gmail.com>
2025-10-20 16:09:32 -0500

    Modificaciones a perfil, organizaciones y eventos

--- FrontEnd/frontend-sigeu/src/app/services/eventos.service.ts
+++ FrontEnd/frontend-sigeu/src/app/services/eventos.service.ts
@@ -3,9 +3,41 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API_PATHS, buildApiUrl } from '../config/config';
 
-export interface Evento {
-  id?: number;
+export interface EventoRegistroCompleto {
+  nombre: string;
+  descripcion?: string;
+  tipo: string;
+  fecha: string;
+  hora_inicio: string;
+  hora_fin: string;
+  id_usuario_registra: number;
+  organizaciones: OrganizacionDTO[];
+  responsables: ResponsableDTO[];
+  reservaciones: ReservacionDTO[];
+}
+
+export interface OrganizacionDTO {
+  nit: string;
   nombre?: string;
+  representante_legal?: string;
+  ubicacion?: string;
+  telefono?: string;
+  sector_economico?: string;
+  actividad_principal?: string;
+  certificado_participacion?: string;
+  representante_alterno?: string;
+}
+
+export interface ResponsableDTO {
+  id_usuario: number;
+  documentoAval?: string;
+  tipoAval?: string;
+}
+
+export interface ReservacionDTO {
+  codigo_espacio: string;
+  hora_inicio: string;
+  hora_fin: string;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -17,7 +49,7 @@ export class EventosService {
     return this.http.get(`${this.baseUrl}/listar`);
   }
 
-  registrar(evento: Evento): Observable<any> {
+  registrar(evento: EventoRegistroCompleto): Observable<any> {
     return this.http.post(`${this.baseUrl}/registrar`, evento);
   }
 
